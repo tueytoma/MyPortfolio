de487b29ba98e7da4602b20c3c28e5b8e30d8185
Tuey Sitthichai <tueytobi@gmail.com>
2020-02-02 23:05:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './TMRoomLogo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TueyToma Webiste. :)</h1>
+          <h1 className="App-title">Welcome to <span className="App-big">ToMaRoom</span> ...</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
